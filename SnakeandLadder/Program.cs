e6e9e681834ae865337663597893ecb44c94aac3
karanmahi71 <karankasbe7171@gmail.com>
2022-08-24 23:48:01 +0530

    Single player with start position

---
+++ SnakeandLadder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeandLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder problem statement");
+        }
+    }
+}
